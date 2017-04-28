d47328a4ae7d8885f656e51fd1d9789315206609
lmislm <lmjustlm@163.com>
2017-04-28 22:23:16 +0800

    send函数}bug

--- Http_Demo/tweet_client.js
+++ Http_Demo/tweet_client.js
@@ -6,21 +6,21 @@ var http = require('http'),
     //发送数据
     function send(theName) {
         http.request({
-            host:   '127.0.0.1',
-            port:   3000,
-            url:    '/',
+            host: '127.0.0.1',
+            port: 3000,
+            url: '/',
             method: 'POST'
-        },function (res) {
+        }, function (res) {
             res.setEncoding('utf8');
             res.on('end', function () {
                 console.log('\n \033[90m    request complete!\033[39m');
-        process.stdout.write('\n    your name:  ');
+                process.stdout.write('\n    your name:  ');
             });
-        }).end(qs.stringify({   name: theName   }));
+        }).end(qs.stringify({name: theName}));
+    }
         process.stdout.write('\n    your name:  ');
         process.stdin.resume();
         process.stdin.setEncoding('utf-8');
         process.stdin.on('data',    function (name) {
             send(name.replace('\n',''));
         })
-    }
