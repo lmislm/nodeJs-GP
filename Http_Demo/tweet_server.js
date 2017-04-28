9e41afc90fa93e554ce2c491d3e2b9f01f8482cc
lmislm <lmjustlm@163.com>
2017-04-28 21:14:25 +0800

    web添加客户端和服务端

---
+++ Http_Demo/tweet_server.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/4/28.
+ */
+var qs = require('querystring');
+require('http').createServer(function (req, res) {
+    var body = '';
+    req.on('data',function (chunk) {
+        body += chunk;
+    });
+    req.on('end', function () {
+        res.writeHead(200);
+        res.end('Done');
+        //parse请求内容解析
+        console.log('\n Got name \033[90m' + qs.parse(body).name + '\033[90m');
+    })
+})
