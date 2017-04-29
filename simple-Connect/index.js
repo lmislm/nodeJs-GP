41f3bf8d7f410e3a7e0ad6b5da8599bfc58bfdac
lmislm <lmjustlm@163.com>
2017-04-29 10:24:36 +0800

    构建connect前的代码

--- simple-Connect/index.js
+++ simple-Connect/index.js
@@ -1,3 +1,40 @@
 /**
  * Created by Administrator on 2017/4/29.
  */
+var http = require('http'),
+    fs = require('fs')
+
+var server = http.createServer(function (req, res) {
+    if('GET' == req.method && '/images' == req.url.substr(0, 7)
+        && '.jpg' == req.url.substr(-4)){
+        fs.stat(__dirname + req.url, function (err, stat) {
+            if(err || !stat.isFile){
+                res.writeHead(404);
+                res.end('Not Found');
+                return;
+            }
+            server(__dirname + req.url, 'application/jpg');
+        });
+    }else if('GET' == req.method && '/' == req.url){
+        serve()
+    }else{
+        res.writeHead(404);
+        res.end('Not Found');
+    }
+    function serve(path, type) {
+        res.writeHead(200, {'Content-Type':type});
+        fs.createReadStream(path).pipe(res);
+    }
+});
+server.listen(3000);
+// var connect = require('connect')
+// /*
+// * 创建服务器
+// * */
+// var server = connect.createServer();
+// /*
+// * 处理中间件
+// * */
+// // server.use(connect.static(__dirname + '/website'));
+// server.use(connect.static(__dirname + './website'));
+// server.listen(3000);
